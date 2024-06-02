410a8bf10d020b6bfb9d12fff5d8e53ea4aa6390
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-06-02 08:55:57 +0530

    Mobile Menu

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import React from 'react'
+import Menu from './Menu'
+
+const Navbar = () => {
+  return (
+    <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative'>
+        {/* Mobile */}
+        <div className='h-full flex items-center justify-between'>
+            <Link href="/">
+                <div className='text-2xl tracking-wide'>NexCommerce</div>
+            </Link>
+            <Menu />
+        </div>
+        {/* Bigger Screens */}
+    </div>
+  )
+}
+
+export default Navbar
