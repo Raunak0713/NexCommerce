12c03a186745c3edf04465863263493a8a9e29da
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-06-03 08:09:01 +0530

    new

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
 import React from 'react'
 import Menu from './Menu'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative'>
+
         {/* Mobile */}
         <div className='h-full flex items-center justify-between'>
             <Link href="/">
@@ -12,7 +14,24 @@ const Navbar = () => {
             </Link>
             <Menu />
         </div>
+
         {/* Bigger Screens */}
+        <div className='hidden md:flex items-center justify-between h-full gap-8'>
+
+            {/* Left */}
+            <div className='w-1/3'>
+                <Link href='/'>
+                    <Image src="/logo.png" alt="" width={24} height={24}/>
+                </Link>
+                <div className='text-2xl tracking-wide'>NexCommerce</div>
+            </div>
+
+            {/* Right */}
+            <div className='w-2/3'>
+
+            </div>
+
+        </div>
     </div>
   )
 }
